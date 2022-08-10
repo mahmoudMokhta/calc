3df35c340bb52a370a068c71db7d9423743cf143
mahmoudMokhta <mailto:mahmoud66mokhtar@gmail.com>
2022-08-10 22:13:37 +0200

    first

---
+++ javascript/js.js
@@ -0,0 +1,71 @@
+var btn   = document.querySelectorAll('.btn'),
+    
+    result=document.querySelector('.result'),
+
+    clear =document.querySelector('.clear'),
+
+    equal =document.querySelector('.equal'),
+
+    clearEnd = document.querySelector('.clear-end'),
+
+    btnColor = document.querySelectorAll('.b-color'),
+
+    container = document.querySelector('.container')
+
+   
+    btnColor.forEach((button)=>{
+
+        button.addEventListener('click',(atrr)=>{
+
+            container.style.backgroundColor = atrr.target.dataset.color;
+        }
+
+        )} );
+
+
+    btn.forEach((button)=>{
+
+        button.addEventListener('click',()=>{
+        //    console.log(button.value)
+        
+        result.value += button.value
+    
+        })
+    })
+    
+    clear.addEventListener('click', ()=> {
+    
+        result.value = ""
+    })
+
+
+    equal.addEventListener('click',()=>{
+
+        result.value = eval(result.value)
+
+    })
+
+
+    clearEnd.addEventListener('click',()=>{
+
+        result.value =  result.value.slice(0 , -1)
+
+    })
+        
+
+  
+
+    
+
+
+ 
+
+
+
+
+
+
+
+
+
+
